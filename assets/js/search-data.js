15874afe2e3d32f4a6c4dee17b5999e7b583e771
Landbus <60614195+Landbus@users.noreply.github.com>
2026-02-16 03:18:30 +0000

    Deploying to gh-pages from @ Landbus/landbus.github.io@746fa8e47aba7e38bd06d1c9d928dabc6228417f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "[]",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
